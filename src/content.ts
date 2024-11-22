5c2f2d0e1cb8b7e96b21b04fd9dfb64cb8c856c2
Pascal Flores <pascalthomas.flores@proton.me>
2024-11-22 14:40:36 +0100

    implemented function to get all resources links from learn page

--- src/content.ts
+++ src/content.ts
@@ -1,12 +1,39 @@
 import { Action } from "./modules/Action";
+import { Resource } from "./modules/Resource";
 
-chrome.runtime.onMessage.addListener((message : Action) => {
+chrome.runtime.onMessage.addListener((message : Action, _, sendResponse) => {
     switch (message) {
         case Action.getAllResources:
-            getAllResources()
+            getAllResources().then((resources: string[]) => {
+                sendResponse(resources)
+            })
     }
+    return true
 });
 
-function getAllResources () {
-    console.log('not implemented')
+function generateResourceLink(resourceId: number) : string {
+    return `https://fr.vittascience.com/learn/tutorial.php?id=${resourceId}`
+}
+
+async function getAllResources () : Promise<string[]>{
+    let page = 1
+    let resources : string[] = []
+    while (true) {
+        let bodyData = new URLSearchParams()
+        bodyData.append("page", page.toString())
+
+        let pageResources : Resource[] = (await (await fetch("https://fr.vittascience.com/routing/Routing.php?controller=course&action=get_by_filter", {
+            method  : "POST",
+            headers : {"Content-Type" : "application/x-www-form-urlencoded"},
+            body    : bodyData
+        })).json())
+        if (!pageResources || pageResources.length === 0) break;
+        else {
+            for (let pageResource of pageResources) {
+                resources.push(generateResourceLink(pageResource["id"]))
+            }
+        }
+        page += 1
+    }
+    return resources
 }
