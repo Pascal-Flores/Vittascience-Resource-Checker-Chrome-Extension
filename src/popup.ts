2f5e5b325a5739baf49a9105f7cd8ea7c831832e
Pascal Flores <pascalthomas.flores@proton.me>
2024-11-25 10:11:55 +0100

    added buttons and functions to check one or all resources

--- src/popup.ts
+++ src/popup.ts
@@ -4,8 +4,13 @@ document.addEventListener("DOMContentLoaded", async () => {
     let tabUrl = (await getCurrentTab()).url!
     if (tabUrl?.match(/https:\/\/.*\.vittascience\.com\/learn.*/gm)?.length != null) {
         document.querySelector("#popup-content")!.innerHTML = `
-        <button id="scan">Lancer un scan des ressources</button>`
-        document.querySelector("#scan")?.addEventListener("click", getAllResources)
+        <button id="scan">Lancer un scan des ressources</button>
+        <button id="checkResource" />Checker la première ressource de la page</button>
+        <button id="checkAllResources">Checker toutes les ressources</button>
+        `
+        document.querySelector("#scan")!.addEventListener("click", getAllResources)
+        document.querySelector("#checkResource")!.addEventListener("click", checkResource)
+        document.querySelector("#checkAllResources")!.addEventListener("click", checkAllResources)
     }
     else {
         document.querySelector("#popup-content")!.innerHTML = `
@@ -23,4 +28,12 @@ async function getCurrentTab() : Promise<chrome.tabs.Tab> {
 async function getAllResources() {
     const resources = await chrome.tabs.sendMessage((await getCurrentTab()).id!, Action.getAllResources)
     alert(`${resources.length} ressources ont été trouvées.`)
+}
+
+async function checkResource() {
+    await chrome.tabs.sendMessage((await getCurrentTab()).id!, Action.checkResource)
+}
+
+async function checkAllResources() {
+    await chrome.tabs.sendMessage((await getCurrentTab()).id!, Action.checkAllResources)
 }
